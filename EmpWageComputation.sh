5e0733deb949c609a66db9a54bea68a407d14bc7
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-10 20:13:40 +0530

    UC-1:Added Emp Attendance

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
 echo "Welcome to Employee Wage Problem"
+
+isPresent=$(( RANDOM%2 ))
+
+if [ $isPresent -eq 1 ]
+then
+	echo "Employee is present"
+else
+	 echo "Employee is not present"
+fi
